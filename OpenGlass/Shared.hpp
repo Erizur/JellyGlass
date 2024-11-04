454204013530a3e0a8fc19f7a6a50fdc96a6ab42
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2024-11-04 12:15:35 -0500

    Update Shared.hpp

--- OpenGlass/Shared.hpp
+++ OpenGlass/Shared.hpp
@@ -11,8 +11,8 @@ namespace OpenGlass::Shared
 		DWORD tbWidth = 0;
 		MARGINS textInset = { 0 };
 		bool render = false;
-		IDWriteTextFormat* textFormat = nullptr;
 		IDWriteTextLayout* textLayout = nullptr;
+		IDWriteTextFormat* textFormat = nullptr;
 		RECT fillboxSize = { 0 };
 		bool isActive = true; // this is used mainly to check for re-rendering when window state changes
 	} TEXTEX;
